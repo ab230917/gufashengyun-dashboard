b842a25b4ebbd96eded0b1f8d57a01fbbfc38e7f
ab230917 <845323671@qq.com>
2026-09-26 17:52:34 +0800

    更新9月销售看板数据 20260926

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 const DASHBOARD_DATA = {
-  "lastUpdated": "2026-09-25T17:53:35.361389+08:00",
+  "lastUpdated": "2026-09-26T17:52:15.287551+08:00",
   "targets": {
     "total": 600000,
     "regular": 371396,
